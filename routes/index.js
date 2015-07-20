9942419bd987fe4a7ea647c6e5699768c034dd56
natxomi <natxo.miranda@gmail.com>
2015-07-20 16:28:28 +0200

    Autores

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+/** GET AUTHOR    */
+router.get('/author', function(req, res) {
+  res.render('author', { author: 'Natxo Miranda' });
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
