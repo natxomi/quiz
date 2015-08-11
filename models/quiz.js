c234e215fa295f2dec1d3dd3d86db1f442c36a19
natxomi <natxo.miranda@gmail.com>
2015-08-11 19:50:22 +0200

    multiples preguntas

---
+++ models/quiz.js
@@ -0,0 +1,8 @@
+// Definición del módulo de Quiz
+
+module.exports = function(sequelize, DataTypes) {
+   return sequelize.define('Quiz',
+             { pregunta:  DataTypes.STRING,
+			   respuesta: DataTypes.STRING,
+             });
+}
