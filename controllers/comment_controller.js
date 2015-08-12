ac81344d10a6de340124b546840c0cd544d58d5b
natxomi <natxo.miranda@gmail.com>
2015-08-12 13:20:29 +0200

    Crear comentario

---
+++ controllers/comment_controller.js
@@ -0,0 +1,28 @@
+var models = require('../models/models.js');
+
+// GET /quizes/:quizId/comments/new
+exports.new = function(req, res) {
+  res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
+};
+
+// POST /quizes/:quizId/comments
+exports.create = function(req, res) {
+  var comment = models.Comment.build(
+      { texto: req.body.comment.texto,          
+        QuizId: req.params.quizId
+        });
+		var errors =  comment.validate();
+ 
+ if (errors)
+		{
+		var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+for (var prop in errors) errores[i++]={message: errors[prop]}; 
+        res.render('comments/new.ejs', {comment: comment, errors: err.errors});
+      } 
+ else {
+        comment // save: guarda en DB campo texto de comment
+        .save()
+        .then( function(){ res.redirect('/quizes/'+req.params.quizId)}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+ 
