fcdb70052b6bdd21ba5291568c957d328851031c
natxomi <natxo.miranda@gmail.com>
2015-08-12 13:33:12 +0200

    Moderar preguntas

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,32 @@
 var models = require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId) {
+  models.Comment.find({
+            where: {
+                id: Number(commentId)
+            }
+        }).then(function(comment) {
+      if (comment) {
+        req.comment = comment;
+        next();
+      } else{next(new Error('No existe commentId=' + commentId))}
+    }
+  ).catch(function(error){next(error)});
+};
+
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+  req.comment.publicado = true;
+
+  req.comment.save( {fields: ["publicado"]})
+    .then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
+    .catch(function(error){next(error)});
+
+  };
+  
+  
 // GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
   res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
@@ -11,18 +38,19 @@ exports.create = function(req, res) {
       { texto: req.body.comment.texto,          
         QuizId: req.params.quizId
         });
-		var errors =  comment.validate();
- 
- if (errors)
-		{
-		var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
-for (var prop in errors) errores[i++]={message: errors[prop]}; 
+
+  comment
+  .validate()
+  .then(
+    function(err){
+      if (err) {
         res.render('comments/new.ejs', {comment: comment, errors: err.errors});
-      } 
- else {
+      } else {
         comment // save: guarda en DB campo texto de comment
         .save()
         .then( function(){ res.redirect('/quizes/'+req.params.quizId)}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
- 
+  ).catch(function(error){next(error)});
+  
+};
